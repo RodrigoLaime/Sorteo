990034faae1df4065cc0ad24265a32894d621328
Rodrigo Laime <rodrigolaime141@gmail.com>
2022-10-10 09:17:32 -0300

    primer commit

---
+++ main.js
@@ -0,0 +1,8 @@
+
+import sorteoDos from "./sorteo.js";
+
+const d = document;
+
+d.addEventListener("DOMContentLoaded", () => {
+  sorteoDos('sorteo-dos', 'agregar-jugador', 'ganador-btn-dos','lista-jugadores');
+});
---
+++ sorteo.js
@@ -0,0 +1,52 @@
+const d = document;
+
+d.addEventListener("DOMContentLoaded", () => {
+    sorteoDos('sorteo-dos', 'agregar-jugador', 'ganador-btn-dos','lista-jugadores');
+  });
+
+function sorteoDos(input, agregar, ganador, jugadores) { //agregamos 
+  const $input = document.getElementById(input), //$ indica que estas trabajando en una etiqueta HTML
+    $agregar = d.getElementById(agregar),
+    $ganador = d.getElementById(ganador),
+    $jugadores = d.getElementById(jugadores);
+  let jugadoresArray = [];
+
+  const agregarJugadores = () => {
+    const inputValue = $input.value;
+
+    if (inputValue === '' || inputValue.length === 0) {
+      alert('No has ingresado participante');
+    } else {
+      jugadoresArray.push(inputValue);
+      $jugadores.insertAdjacentHTML("beforeend", `<li>${inputValue}</li>`);
+      $input.value = '';
+    }
+   };
+  const ganadorSorteo = () => { 
+    $input.focus();
+
+    const random = Math.floor(Math.random() * 
+    
+      jugadoresArray.length)
+    const jugadorGanador = jugadoresArray[random];
+
+    jugadoresArray = [];
+
+    setTimeout(() => {
+      $jugadores.innerHTML = "";
+    }, 2000);
+
+    Swal.fire(`${jugadorGanador} fue seleccionado`);
+  };
+
+  $agregar.addEventListener('click', () => {
+    agregarJugadores()
+   });
+  $ganador.addEventListener('click', () => {
+    if (jugadoresArray.length === 0) {
+      alert('No has ingresado participantes');
+    } else {
+      ganadorSorteo();
+    }
+  });
+}
